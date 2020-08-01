6fc4475d8ef2ebe639b86930939f72170f174751
Swati Suresh <65075557+Luckyfox1111@users.noreply.github.com>
2020-08-01 15:38:13 -0500

    Login is working

--- main.js
+++ main.js
@@ -8,15 +8,28 @@ function closeForm() {
 }
 
 function check(){
-var name=document.getElementById("uname");
-var pass=document.getElementById("psw");
-if(name=="Priya"|| name=="Swati"){
-  if(pass=="12457"){
-    return true
-}
+const name=document.getElementById("uname").value;
+const pass=document.getElementById("psw").value;
+if((name=="Priya"|| name=="Swati")&&pass==12457){
+  window.stop();
+   window.location.assign("develop.html");
 }
 else{
-  return false
+  alert("Wrong username or Password");
 }
 }
 
+function post(){
+  var title=document.getElementById("title").value;
+  console.log(title);
+  var author=document.getElementById("ath").value;
+  console.log(author);
+  var text=document.getElementById("text").value;
+  console.log(text);
+  var option=document.getElementById("choose").value;
+  console.log(option);
+
+  if(option=="Personal"){
+    
+  }
+}
